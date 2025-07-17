67494d69f4ba75d43034670e8f012738454c625a
DEV <DEV@DEV.COM>
2025-07-17 14:12:13 +0530

    commit

--- demos/Java-Core-Demos/src/com/demo/collections/MapEx.java
+++ demos/Java-Core-Demos/src/com/demo/collections/MapEx.java
@@ -9,12 +9,19 @@ public class MapEx {
         m1();
     }
     private static void m1() {
+        // Maps have unique keys and can have duplicate values.
         Map<Object,Object> map1 = new HashMap<Object,Object>();
         Laptop laptop1 = new Laptop("Inspirion", 10);
         Laptop laptop2 = new Laptop("Thinkpad", 10);
         map1.put("HP", laptop1);
         String laptopBrand = "Lenovo";
         map1.put(laptopBrand, laptop2);
+        map1.put("HP", new Laptop("Pavillion", 10));
+        map1.put("null", new Laptop("Tuf", 10));
+        map1.put(null, new Laptop("Rog", 10));
+        map1.put(null, new Laptop("MacBook", 10));
+        map1.put("null", new Laptop("Vaio", 10));
+        map1.put("Sony", new Laptop("Vaio", 10));
         System.out.println(map1);
     }
 }
