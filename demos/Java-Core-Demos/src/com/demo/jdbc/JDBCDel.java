08af9a3df673cbb5528f4763fa41c5a6f45bbe16
DEV <DEV@DEV.COM>
2025-07-22 12:01:13 +0530

    commit

--- demos/Java-Core-Demos/src/com/demo/jdbc/JDBCDel.java
+++ demos/Java-Core-Demos/src/com/demo/jdbc/JDBCDel.java
@@ -4,6 +4,7 @@ public class JDBCDel {
     public static void main(String[] args) {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            System.out.println("Driver Loaded!");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
