b48f29b42f22cf903582539e453e5ca7ebbe1e6b
DEV <DEV@DEV.COM>
2025-07-22 12:59:32 +0530

    commit

--- demos/Java-Core-Demos/src/com/demo/jdbc/JDBCDel.java
+++ demos/Java-Core-Demos/src/com/demo/jdbc/JDBCDel.java
@@ -5,6 +5,22 @@ public class JDBCDel {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             System.out.println("Driver Loaded!");
+
+            // Use Windows Authentication
+            // String url =
+            // "jdbc:sqlserver://localhost:1433;databaseName=TestDB;integratedSecurity=true;";
+            // String url =
+            // "jdbc:sqlserver://localhost:1433;databaseName=TestDB;user=sa;password=sqlserver;";
+            // jdbc is the protocol used to connect to the database
+            // sqlserver is the subprotocol used for SQL Server
+            // localhost is the server name
+            // 1433 is the default port for SQL Server
+            // TestDB is the name of the database
+            // user is the username to connect to the database
+            // password is the password for the user
+            // trustServerCertificate=true is used to bypass SSL certificate validation
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=TestDB;user=sa;password=sqlserver;trustServerCertificate=true;";
+
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
