630e1c5b2a1d9acbb07690c53fcfe03396d639f7
DEV <DEV@DEV.COM>
2025-08-06 15:52:33 +0530

    commit

---
+++ demos/springboot/ld-sb-rest-jpa-hib-crud-mvn-r1/src/main/java/com/demo/entity/Employee.java
@@ -0,0 +1,62 @@
+package com.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+    @Column(name = "EMAIL_ADDRESS", nullable = false)
+    private String emailId;
+
+    // Default constructor
+    public Employee() {
+    }
+
+    // Parameterized constructor
+    public Employee(Long id, String firstName, String lastName, String emailId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
