20febf4ea23eedbcc13d0861cc95242fca8a76b3
DEV <DEV@DEV.COM>
2025-08-07 16:27:38 +0530

    commit

--- demos/springboot/ld-sb-rest-jpa-hib-crud-mvn-r1/src/main/java/com/demo/App.java
+++ demos/springboot/ld-sb-rest-jpa-hib-crud-mvn-r1/src/main/java/com/demo/App.java
@@ -1,7 +1,12 @@
 package com.demo;
 
+import com.demo.entity.EmployeeEntity;
+import com.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class App 
@@ -11,5 +16,14 @@ public class App
         System.out.println("Welcome to our Java App!");
         SpringApplication.run(App.class, args);
         System.err.println("Welcome to our Spring App!");
+//        System.out.println(EmployeeData.getAllEmployees());
     }
 }
+
+//class EmployeeData {
+//    @Autowired
+//    static EmployeeService employeeService;
+//    public static List<EmployeeEntity> getAllEmployees(){
+//        return employeeService.getAllEmployees();
+//    }
+//}
