78349f55066dcfe84a36c8c1b13768785e9b5afc
DEV <DEV@DEV.COM>
2025-08-20 12:30:48 +0530

    commit

--- demos/ld1a-JAVA-21-r1/src/main/java/com/demo/java17/m1diamond/DiamondOperatorDemo.java
+++ demos/ld1a-JAVA-21-r1/src/main/java/com/demo/java17/m1diamond/DiamondOperatorDemo.java
@@ -2,6 +2,7 @@ package com.demo.java17.m1diamond;
 
 import java.math.BigDecimal;
 // BigDecimal is used for precise financial calculations
+// The BigDecimal 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,11 +31,15 @@ public class DiamondOperatorDemo {
         // Legacy [Pre Java 7]
         Map<String, List<Transaction>> legacyTransactions = new HashMap<String, List<Transaction>>();
         legacyTransactions.put("USD", new ArrayList<Transaction>());
-
+        // Add a transaction to the USD list
+        // legacyTransactions.get("USD").add(new Transaction("USD1", BigDecimal.valueOf(100.50)));
+        List<Transaction> usdTransactions = legacyTransactions.get("USD");
+        usdTransactions.add(new Transaction("USD1", BigDecimal.valueOf(100.50)));
+        System.err.println(legacyTransactions);
         // Legacy [Java 7] allows type inference
         Map<String, List<Transaction>> legacyTransactions1 = new HashMap<>();
         legacyTransactions1.put("EUR", new ArrayList<Transaction>());
-
+        System.out.println(legacyTransactions1);
         // Java 7
         TransactionProcessor<Transaction> legacyProcessor = new TransactionProcessor<Transaction>() {
             @Override
@@ -42,6 +47,7 @@ public class DiamondOperatorDemo {
                 System.out.println("Modern Transaction: " + transaction);
             }
         };
+        legacyProcessor.process(new Transaction("JPY", BigDecimal.valueOf(500.5f)));
         // Java 9+ Diamond operator with anonymous inner class
         TransactionProcessor<Transaction> modernProcessor = new TransactionProcessor<>() {
             @Override
@@ -49,6 +55,7 @@ public class DiamondOperatorDemo {
                 System.out.println("Modern Transaction: " + transaction);
             }
         };
+        modernProcessor.process(new Transaction("GBP", BigDecimal.valueOf(1000.75f)));
     }
 }
 
