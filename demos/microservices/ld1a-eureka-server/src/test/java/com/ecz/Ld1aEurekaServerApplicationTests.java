ec31bafaf83643f178a2f6094fb1a4cc7d4a4498
DEV <DEV@DEV.COM>
2025-08-21 14:39:35 +0530

    commit

---
+++ demos/microservices/ld1a-eureka-server/src/test/java/com/ecz/Ld1aEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ld1aEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
